38677a6ce3ddf4f5b7f845536c364c3981d2d194
eoinpatrickreid <s1858933@ed.ac.uk>
2024-10-13 12:18:13 +0100

    Removed explicit adress reference in axiosInstance

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,23 +2,19 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "/", // Relative to the current domain
 });
 
 // Add a request interceptor to include the JWT token
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token) {
-      if (config.headers) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
