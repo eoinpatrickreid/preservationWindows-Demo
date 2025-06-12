a8104200cbaf41428125ff88e98ce2a04f22de3e
eoinpatrickreid <s1858933@ed.ac.uk>
2025-06-12 18:57:55 +0100

    Test

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
             textAlign="center"
             color="gray.800"
           >
-            Welcome to Preservation Quotes
+            Welcome to Preservation Quotes TEST
           </Heading>
           <Box
             padding="30px" // Add 20px padding
